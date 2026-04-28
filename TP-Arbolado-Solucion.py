2881f5c71f7668632b953f5e7631a40eefbe1d40
r3ch1rd <110585442+r3ch1rd@users.noreply.github.com>
2026-04-28 01:14:55 -0300

    Add files via upload

--- TP-Arbolado-Solucion.py
+++ TP-Arbolado-Solucion.py
@@ -27,10 +27,78 @@ print(len(leer_parque(archivo_arbolado,"GENERAL PAZ")))
 
 def especies(lista_arboles):
     especies_en_lista:list = []
-    for dic in lista_arboles:
-        especies_en_lista.append(dic["nombre_com"])
+    for arbol in lista_arboles:
+        especies_en_lista.append(arbol["nombre_com"])
     return set(especies_en_lista)
 
 print(especies(leer_parque(archivo_arbolado,"GENERAL PAZ")))
 
 #Ejercicio 3
+
+def contar_ejemplares(lista_arboles):
+    cantidad_ejemplares:dict = {}
+    for arbol in lista_arboles:
+        if arbol["nombre_com"] in cantidad_ejemplares:
+            cantidad_ejemplares[arbol["nombre_com"]] += 1
+        else:
+            cantidad_ejemplares[arbol["nombre_com"]] = 1
+    return cantidad_ejemplares
+
+general_paz = leer_parque(archivo_arbolado,"GENERAL PAZ")
+ejemplares_general = contar_ejemplares(general_paz)
+
+print(ejemplares_general["Jacarandá"])
+
+#Ejercicio 4
+
+def obtener_alturas(lista_arboles,especie):
+    lista_alturas:list = []
+    for arbol in lista_arboles:
+        if arbol["nombre_com"] == especie:
+            lista_alturas.append((float(arbol["altura_tot"])))
+    return lista_alturas
+
+def prom(lista):
+    prom:int = 0
+    for i in lista:
+        prom += i
+    prom = prom/len(lista)
+    return prom
+print(max(obtener_alturas(general_paz,"Jacarandá")),prom(obtener_alturas(general_paz,"Jacarandá")))
+
+#Ejercicio 5
+
+def obtener_inclinaciones(lista_arboles, especie):
+    inclinaciones:list = []
+    for arbol in lista_arboles:
+        if arbol["nombre_com"] == especie:
+            inclinaciones.append(float(arbol["inclinacio"]))
+    return inclinaciones
+
+#Ejercicio 6
+
+def especimen_mas_incliado(lista_arboles):
+    mas_inclinado_esp:str
+    mas_inclinado_inc:int = 0
+    for especie in especies(lista_arboles):
+        if max(obtener_inclinaciones(lista_arboles,especie))>= mas_inclinado_inc:
+            mas_inclinado_esp = especie
+            mas_inclinado_inc = max(obtener_inclinaciones(lista_arboles,especie))
+    return mas_inclinado_esp, mas_inclinado_inc
+
+parque_centenario = leer_parque(archivo_arbolado,"CENTENARIO")
+print(especimen_mas_incliado(parque_centenario))
+
+#Ejercicio 7
+
+def especie_promedio_mas_inclinada(lista_arboles):
+    mayor_inclinacion_esp:str
+    mayor_inclinacion_prom:int = 0
+    for especie in especies(lista_arboles):
+        if prom(obtener_inclinaciones(lista_arboles,especie))>=mayor_inclinacion_prom:
+            mayor_inclinacion_prom = prom(obtener_inclinaciones(lista_arboles,especie))
+            mayor_inclinacion_esp = especie
+    return mayor_inclinacion_esp, mayor_inclinacion_prom
+
+parq_los_andes = leer_parque(archivo_arbolado,"ANDES, LOS")
+print(especie_promedio_mas_inclinada(parq_los_andes))
